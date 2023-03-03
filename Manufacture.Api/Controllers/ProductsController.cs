8dbe890a762981cc35de22f569f544c1d07fed3f
amir.shahzad <amir.shahzad1978@gmail.com>
2023-03-03 10:58:55 +0500

    database and First API Completed and Tested

---
+++ Manufacture.Api/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using Manufacture.Api.Data;
+using Manufacture.Api.Data.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Manufacture.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ManufactureContext _context;
+
+        public ProductsController(ManufactureContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> GetAsync(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostAsync(Products newProduct )
+        {
+            _context.Products.Add(newProduct);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("Get", new {id=newProduct.Id},newProduct);
+        }
+    }
+}
